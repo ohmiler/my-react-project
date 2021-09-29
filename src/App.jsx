02daa3b78e7b03378abad5a303377ecdc9b27bb8
Patiphan Phengpao <misterpatipan123@gmail.com>
2021-09-29 11:48:42 +0700

    second edit in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>Increase</button>
       <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count + 10)}>Increase by ten</button>
     </div>
   )
 }
